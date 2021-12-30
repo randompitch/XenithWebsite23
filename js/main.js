7dc74024caa80378b8cab3214585b77b5f83f1ba
Avni17 <avni.arora2002@gmail.com>
2021-12-30 12:09:52 +0530

    avni

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
-$(document).ready(function() {
- 
-    setTimeout(function(){
+$(document).ready(function () {
+
+    setTimeout(function () {
         $('#preLoader').addClass('uk-hidden');
         $('#postLoader').removeClass('uk-hidden');
     }, 3000);
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var nav = $('#navbarMain');
         var top = 200;
         if ($(window).scrollTop() >= top) {
             nav.addClass('background-xenith');
-    
+
         } else {
             nav.removeClass('background-xenith');
         }
     });
 
 });
+const signs = document.querySelectorAll('x-sign')
+const randomIn = (min, max) => (
+    Math.floor(Math.random() * (max - min + 1) + min)
+)
+
+const mixupInterval = el => {
+    const ms = randomIn(2000, 4000)
+    el.style.setProperty('--interval', `${ms}ms`)
+}
+
+signs.forEach(el => {
+    mixupInterval(el)
+    el.addEventListener('webkitAnimationIteration', () => {
+        mixupInterval(el)
+    })
+})
