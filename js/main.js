d9de72db050447a23ce026e969545744124c7b96
Avni17 <avni.arora2002@gmail.com>
2021-12-31 00:59:15 +0530

    a

--- js/main.js
+++ js/main.js
@@ -35,3 +35,27 @@ signs.forEach(el => {
         mixupInterval(el)
     })
 })
+const card = document.querySelector(".card");
+const motionMatchMedia = window.matchMedia("(prefers-reduced-motion)");
+const THRESHOLD = 15;
+
+function handleHover(e) {
+    const { clientX, clientY, currentTarget } = e;
+    const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+
+    const horizontal = (clientX - offsetLeft) / clientWidth;
+    const vertical = (clientY - offsetTop) / clientHeight;
+    const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+    const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+
+    card.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+}
+
+function resetStyles(e) {
+    card.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+}
+
+if (!motionMatchMedia.matches) {
+    card.addEventListener("mousemove", handleHover);
+    card.addEventListener("mouseleave", resetStyles);
+}
