afa566b5d33f736f124744138c2086b1d6f0c270
Avni17 <avni.arora2002@gmail.com>
2022-01-01 19:37:53 +0530

    avni

--- js/main.js
+++ js/main.js
@@ -36,29 +36,5 @@ signs.forEach(el => {
     })
 })
 
-const motionMatchMedia = window.matchMedia("(prefers-reduced-motion)");
-const THRESHOLD = 2;
-if (!motionMatchMedia.matches) {
-    const card = document.querySelectorAll('.image-card').forEach(item => {
-        item.addEventListener('mousemove', event => {
-
-            const { clientX, clientY, currentTarget } = event;
-            const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
-            const xVal = event.layerX;
-            const yVal = event.layerY;
-            const rotateX = 20 * ((xVal - clientWidth / 2) / clientWidth);
-            const rotateY = -20 * ((yVal - clientHeight / 2) / clientHeight);
-            item.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
-
-
-
-        })
-        item.addEventListener('mouseleave', event => {
-
-            item.style.transform = `perspective(${event.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
-
-        })
-    });
-}
 
 
