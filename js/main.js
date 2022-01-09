cc2f789d5c3d2c44a9f7f06a836fa4c6846cb384
Avni17 <avni.arora2002@gmail.com>
2022-01-09 17:39:19 +0530

    avni

--- js/main.js
+++ js/main.js
@@ -52,3 +52,34 @@ var refreshIntervalId = setInterval(function(){
     }
 });
 clearInterval(refreshIntervalId);},4000);
+var refresh = setInterval(function(){
+    (function () {
+        "use strict";
+        var items = document.querySelectorAll(".timeline li");
+        function isElementInViewport(el) {
+          var rect = el.getBoundingClientRect();
+          return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+              (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+          );
+        }
+      
+        function callbackFunc() {
+          for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+              items[i].classList.add("in-view");
+            }
+          }
+        }
+      
+        // listen for events
+        window.addEventListener("load", callbackFunc);
+        window.addEventListener("resize", callbackFunc);
+        window.addEventListener("scroll", callbackFunc);
+      })();
+clearInterval(refresh);},4000);
+
+  
