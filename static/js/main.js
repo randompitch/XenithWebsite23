6f4b4b415263d14de927840dd573483023ae0a76
Avni17 <avni.arora2002@gmail.com>
2022-01-17 22:50:50 +0530

    a

--- static/js/main.js
+++ static/js/main.js
@@ -82,7 +82,7 @@ fetch("./sponsorData.json")
       createSponsorCard(sponsor.name, sponsor.type, sponsor.image, sponsor.link)
     );
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.log(err));
 // var header = document.getElementById('1');
 
 // function fadeOutOnScroll(element) {
