272bef2dbb369e616b6ff385b1de86cab10ad48e
Avni17 <avni.arora2002@gmail.com>
2023-01-22 18:34:24 +0530

    music autoplay mob try

--- static/js/main.js
+++ static/js/main.js
@@ -224,22 +224,3 @@ function callbackFunc() {
 
  window .addEventListener("load", callbackFunc);
  window.addEventListener("scroll", callbackFunc);
-
-    window.addEventListener('load', function () {
-        var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        var source = audioCtx.createBufferSource();
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'audio-autoplay.wav');
-        xhr.responseType = 'arraybuffer';
-        xhr.addEventListener('load', function (r) {
-            audioCtx.decodeAudioData(
-                    xhr.response, 
-                    function (buffer) {
-                        source.buffer = buffer;
-                        source.connect(audioCtx.destination);
-                        source.loop = false;
-                    });
-            source.start(0);
-        });
-        xhr.send();
-    });
