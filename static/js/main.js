52d12e9b99826d4be985191f3b0eb0eaff841959
Avni17 <avni.arora2002@gmail.com>
2023-01-23 21:56:33 +0530

    sponsor speed fix

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   setTimeout(function () {
     $("#preLoader").addClass("uk-hidden");
     $("#postLoader").removeClass("uk-hidden");
-  }, 1500);
+  }, 2200);
 
   $(window).scroll(function () {
     var nav = $("#navbarMain");
