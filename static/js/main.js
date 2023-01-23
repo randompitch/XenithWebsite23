fa466da847e014993261f6b2faab5fd93b36be04
Avni17 <avni.arora2002@gmail.com>
2023-01-23 19:22:01 +0530

    countdown added

--- static/js/main.js
+++ static/js/main.js
@@ -242,3 +242,59 @@ interval=setInterval(function(){
     clearInterval(interval);
   }
 },60);
+
+setInterval(function () {
+  var video = document.getElementById('myVideo');
+  if(Math.floor(video.currentTime)+2==Math.floor(video.duration))
+  {
+    video.currentTime=2;
+  }
+  
+  console.log(video.currentTime);
+  console.log(video.duration);
+}, 1000);
+
+
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy,
+      dayMonth = "02/10/",
+      birthday = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        // if (distance < 0) {
+        //   document.getElementById("headline").innerText = "It's my birthday!";
+        //   document.getElementById("countdown").style.display = "none";
+        //   document.getElementById("content").style.display = "block";
+        //   clearInterval(x);
+        // }
+        //seconds
+      }, 0)
+  }());
