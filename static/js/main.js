5f4b2c6ca269f1b60b217e8f93b5b8ba2a9058e1
Avni17 <avni.arora2002@gmail.com>
2023-01-29 17:01:12 +0530

    Timeline animation fixed

--- static/js/main.js
+++ static/js/main.js
@@ -264,9 +264,9 @@ var items = document.querySelectorAll(".timeline li");
 function isElementInViewport(el){
     var rect = el.getBoundingClientRect();
   return (
-    rect.top >= 0 &&
+    rect.top >= -150 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom <= (window.innerHeight+150 || document.documentElement.clientHeight+150) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
